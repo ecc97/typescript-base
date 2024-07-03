f5c4d27dc0c15712cf6cbb9260e031f95cb37997
ecc97 <carmonaedwin1123@gmail.com>
2024-07-03 07:40:18 -0500

    fix: foearch

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,14 @@
 "use strict";
-const form = document.querySelector(".new-item-form");
-const type = document.querySelector("#type");
-const toFrom = document.querySelector("#tofrom");
-const details = document.querySelector("#details");
-const amount = document.querySelector("#amount");
-form.addEventListener("submit", (evento) => {
-    evento.preventDefault();
-    console.log(`${type.value.toUpperCase()} es de: ${toFrom.value} details: ${details.value} the amount is: ${amount.valueAsNumber}`);
+// const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// const type = document.querySelector("#type") as HTMLSelectElement;
+// const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+// const details = document.querySelector("#details") as HTMLInputElement;
+// const amount = document.querySelector("#amount") as HTMLInputElement;
+// form.addEventListener("submit", (evento:Event)=> {
+//     evento.preventDefault();
+//     console.log(`${type.value.toUpperCase()} es de: ${toFrom.value} details: ${details.value} the amount is: ${amount.valueAsNumber}`);
+// });
+const inputs = document.querySelectorAll("input");
+inputs.forEach((input) => {
+    console.log(input);
 });
